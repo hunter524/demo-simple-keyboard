a713d8fa44b77a383829faf02b51028320549cb9
hunter <guyuesh2@gmail.com>
2023-07-06 20:27:23 +0800

    html 添加 ios 设备 meta 数据,添加光标移动操作 待添加 Android 设备返回监听,ios 设备 scroll 后 foucus 光标不显示

--- src/App.js
+++ src/App.js
@@ -3,11 +3,44 @@ import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
 import './App.css';
 
+/**
+ * 键盘布局样式
+ * @type {{default: string[]}}
+ */
+const DEFAULT_INPUT_LAYOUT = {
+  "default": ['[1 2 3 4 5 6 7 8 9 . 0 {clear}] [{bksp} {close}]']
+}
+/**
+ * 启用的键盘布局名称
+ * @type {string}
+ */
+const DEFAULT_LAYOUT_NAME = "default"
+
+/**
+ * 针对 clear bksp close 等功能键进行自定义的显示样式和图案
+ * @type {{'{clear}': string, '{close}': string, '{bksp}': string}}
+ */
+const DEFAULT_DISPLAY = {
+  "{clear}": "清空",
+  "{bksp}": "⌫",
+  "{close}":"收起"
+}
+
+/**
+ * 阻止复制粘帖避免 input 和 keyboard 内部状态不一致导致奇怪的问题
+ * @param event
+ * @constructor
+ */
+const STOP_CUT_PASTE = (event) => {
+  event.preventDefault();
+};
+
+let PAGE_COUNT = 0;
+
 class App extends Component {
   state = {
-    layoutName: "ip",
-    inputName: "input1",
-    input: {},
+    inputName: "input1", /*多个 input 标记当前激活的 input 标签,传递给 keyboard 组件*/
+    input: {},/*input name 为 key, value 为输入的值*/
     currentInput:null,
   };
 
@@ -16,28 +49,29 @@ class App extends Component {
       input: inputObj
     });
 
-    console.log("Input changed", inputObj);
+    let input = this.state.currentInput;
+
+    /**
+     * 一定要先直接更新 当前 input 的 value 值
+     * 后续才能更新坐标
+     */
+    input.value = inputObj[this.state.inputName];
+
+    /**
+     * Synchronizing input caret position
+     */
+    let caretPosition = this.keyboard.caretPosition;
+    if (caretPosition !== null) {
+      this.setInputCaretPosition(input, caretPosition);
+    }
   };
 
   onKeyPress = button => {
     console.log("Button pressed", button);
 
-    /**
-     * If you want to handle the shift and caps lock buttons
-     */
-    if (button === "{shift}" || button === "{lock}") this.handleShift();
-
     if (button === "{clear}") this.clearScreen();
 
     if (button === "{close}") this.closeKeyboard();
-  };
-
-  handleShift = () => {
-    let layoutName = this.state.layoutName;
-
-    this.setState({
-      layoutName: layoutName === "default" ? "shift" : "default"
-    });
   };
 
   onChangeInput = event => {
@@ -58,8 +92,12 @@ class App extends Component {
     );
   };
 
-  setActiveInput = (inputName,evt )=> {
 
+  setActiveInput = (inputName,evt )=> {
+    // 监听返回按键
+    if (PAGE_COUNT<=0){
+      window.history.forward()
+    }
     this.setState(
         {
           inputName: inputName,
@@ -69,7 +107,6 @@ class App extends Component {
         () => {
           console.log("Active input", inputName);
           if (evt.target.scrollIntoViewIfNeeded){
-            console.log("call scrollIntoViewIfNeeded")
             evt.target.scrollIntoViewIfNeeded()
           }
           else {
@@ -80,6 +117,9 @@ class App extends Component {
   };
 
   closeKeyboard = () => {
+    if (PAGE_COUNT >= 1){
+      window.history.back()
+    }
     if (this.state.currentInput){
       this.state.currentInput.blur()
     }
@@ -88,12 +128,12 @@ class App extends Component {
     });
   };
 
-  submit = () => {
-    console.log(this.state.input);
-  };
-
-  componentDidMount() {
-
+  setInputCaretPosition(input, pos) {
+    console.log("setInputCaretPosition",input)
+    if (input.setSelectionRange) {
+      input.focus();
+      input.setSelectionRange(pos, pos);
+    }
   }
 
   clearScreen = () => {
@@ -113,140 +153,73 @@ class App extends Component {
     });
   };
 
+  componentDidMount() {
+    window.history.pushState(null,null,document.URL)
+    PAGE_COUNT++
+    window.addEventListener("popstate",()=>{
+      PAGE_COUNT--
+      this.closeKeyboard()
+    })
+  }
+
   render() {
     let { input, keyboardOpen } = this.state;
 
     return (
         <div id='vRoot'>
           <div className="inputsContainer">
-            <div>AAAA<br/>AAAAA</div>
-            <input
-                onFocus={(evt) => this.setActiveInput("input1",evt)}
-                value={input["input1"] || ""}
-                placeholder={"Input 1"}
-                onChange={e => this.onChangeInput(e)}
-                onBlur={this.closeKeyboard}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input2",evt)}
-                value={input["input2"] || ""}
-                placeholder={"Input 2"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input3",evt)}
-                value={input["input3"] || ""}
-                placeholder={"Input 3"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input4",evt)}
-                value={input["input4"] || ""}
-                placeholder={"Input 4"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input5",evt)}
-                value={input["input5"] || ""}
-                placeholder={"Input 5"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input6",evt)}
-                value={input["input6"] || ""}
-                placeholder={"Input 6"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input7",evt)}
-                value={input["input7"] || ""}
-                placeholder={"Input 7"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input8",evt)}
-                value={input["input8"] || ""}
-                placeholder={"Input 8"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input9",evt)}
-                value={input["input9"] || ""}
-                placeholder={"Input 9"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input10",evt)}
-                value={input["input10"] || ""}
-                placeholder={"Input 10"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input11",evt)}
-                value={input["input11"] || ""}
-                placeholder={"Input 11"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input12",evt)}
-                value={input["input12"] || ""}
-                placeholder={"Input 12"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input13",evt)}
-                value={input["input13"] || ""}
-                placeholder={"Input 13"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input14",evt)}
-                value={input["input14"] || ""}
-                placeholder={"Input 14"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input15",evt)}
-                value={input["input15"] || ""}
-                placeholder={"Input 15"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input16",evt)}
-                value={input["input16"] || ""}
-                placeholder={"Input 16"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input17",evt)}
-                value={input["input17"] || ""}
-                placeholder={"Input 17"}
-                onChange={e => this.onChangeInput(e)}
-            />
-            <input
-                onFocus={(evt) => this.setActiveInput("input18",evt)}
-                value={input["input18"] || ""}
-                placeholder={"Input 18"}
-                onChange={e => this.onChangeInput(e)}
-            />
+            <div> 99.5% BROWSER</div>
+            {
+              [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16].map(
+                  value => {
+                    return (<input
+                        id={`input${value}`}
+                        onPaste={STOP_CUT_PASTE}
+                        onCut={STOP_CUT_PASTE}
+                        onFocus={(evt) => this.setActiveInput(`input${value}`,evt)}
+                        value={input[`input${value}`] || ""}
+                        placeholder={`input${value}`}
+                        onChange={e => this.onChangeInput(e)}
+                        onBlur={this.closeKeyboard}
+                        inputMode={'none'}
+                    />)
+                  }
+              )
+            }
+            <div> iOS Before 12.2 osVersion Android Chrome Before 66 Version</div>
+            {
+              [17,18,19,20,21,22,23,24,25,26,27,28].map(
+                  value => {
+                    return (<input
+                        id={`input${value}`}
+                        className={"legacy_outline"}
+                        onPaste={STOP_CUT_PASTE}
+                        onCut={STOP_CUT_PASTE}
+                        onFocus={(evt) => this.setActiveInput(`input${value}`,evt)}
+                        value={input[`input${value}`] || ""}
+                        placeholder={`input${value}`}
+                        onChange={evt => this.onChangeInput(evt)}
+                        onBlur={this.closeKeyboard}
+                        readOnly={true}
+                    />)
+                  }
+              )
+            }
+
           </div>
           <div className={`keyboardContainer ${!keyboardOpen ? "hidden" : ""}`}>
             <Keyboard
                 keyboardRef={r => (this.keyboard = r)}
                 inputName={this.state.inputName}
-                layoutName={this.state.layoutName}
+                layoutName={DEFAULT_LAYOUT_NAME}
+                layout={DEFAULT_INPUT_LAYOUT}
+                display={DEFAULT_DISPLAY}
                 preventMouseDownDefault={true}
                 autoUseTouchEvents={true}
+                inputPattern={/(^\d+$)|(^\d+\.$)|(^\d+\.\d{0,2}$)/}
                 onChangeAll={inputObj => this.onChangeAll(inputObj)}
                 onKeyPress={button => this.onKeyPress(button)}
-                layout={{
-                  "ip": ['[1 2 3 4 5 6 7 8 9 . 0 {clear}] [{bksp} {close}]']
-                }}
-                display={{
-                  "{clear}": "清空",
-                  "{bksp}": "⌫",
-                  "{close}":"收起"
-                }}
+
             />
           </div>
         </div>
